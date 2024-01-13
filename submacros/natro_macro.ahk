1toop/NatroMacroDev

commit 9f02dab50d80ae8ff12d124ab23fc3bd986b000d

Changes
- Fixed nm_reset() (There are two spots it will false positive on... however the chances of it happening are rare and i'm currently working on preventing them)
- Fixed many coord issues
- Added claimhive bitmap
- Changed nm_GoToCannon path

submacros/natro_macro.ahk
@@ -3168,7 +3168,7 @@ mp_PlantPlanter(PlanterIndex) {
 			}
 		}
 
-		if ((Gdip_ImageSearch(pBMScreen, bitmaps[MPlanterName], planterPos, , , 306, , 10, , 5) = 0) || (Gdip_ImageSearch(pBMScreen, bitmaps["yes"], , windowWidth//2-250, , , , 2, , 2) = 1)) {
+		if ((Gdip_ImageSearch(pBMScreen, bitmaps[MPlanterName], planterPos, , , 306, , 10, , 5) = 0) || (Gdip_ImageSearch(pBMScreen, bitmaps["yes"], , WindowWidth//3-30, , , , 2, , 2) = 1)) {
 			Gdip_DisposeImage(pBMScreen)
 			break
 		}
@@ -3181,9 +3181,9 @@ mp_PlantPlanter(PlanterIndex) {
 	{
 		WinGetClientPos(windowX, windowY, windowWidth, windowHeight, "ahk_id " hwnd)
 		loop 3 {
-			pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250 "|" windowY+((6*windowHeight)//10 - 60) "|500|150")
+			pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//3-30 "|" windowY+((5*windowHeight)//10 - 52) "|500|150")
 			if (Gdip_ImageSearch(pBMScreen, bitmaps["yes"], pos, , , , , 2, , 2) = 1) {
-				MouseMove, windowX+windowWidth//2-250+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((6*windowHeight)//10 - 60)+SubStr(pos, InStr(pos, ",")+1)
+				MouseMove, windowX+windowWidth//3-30+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((5*windowHeight)//10 - 52)+SubStr(pos, InStr(pos, ",")+1)
 				sleep, 150
 				Click
 				sleep 100
@@ -3441,9 +3441,9 @@ mp_HarvestPlanter(PlanterIndex) {
 		WinGetClientPos(windowX, windowY, windowWidth, windowHeight, "ahk_id " hwnd)
 		if (PlanterHarvestFull%PlanterIndex% == "Full") {
 			loop 3 {
-				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250 "|" windowY+((6*windowHeight)//10 - 60) "|500|150")
+				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//3-30 "|" windowY+((5*windowHeight)//10 - 52) "|500|150")
 				if (Gdip_ImageSearch(pBMScreen, bitmaps["no"], pos, , , , , 2, , 3) = 1) {
-					MouseMove, windowX+windowWidth//2-250+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((6*windowHeight)//10 - 60)+SubStr(pos, InStr(pos, ",")+1)
+					MouseMove, windowX+windowWidth//3-30+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((5*windowHeight)//10 - 52)+SubStr(pos, InStr(pos, ",")+1)
 					sleep, 150
 					Click
 					sleep 100
@@ -3458,9 +3458,9 @@ mp_HarvestPlanter(PlanterIndex) {
 		}
 		else {
 			loop 3 {
-				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250 "|" windowY+((6*windowHeight)//10 - 60) "|500|150")
+				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//3-30 "|" windowY+((5*windowHeight)//10 - 52) "|500|150")
 				if (Gdip_ImageSearch(pBMScreen, bitmaps["yes"], pos, , , , , 2, , 2) = 1) {
-					MouseMove, windowX+windowWidth//2-250+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((6*windowHeight)//10 - 60)+SubStr(pos, InStr(pos, ",")+1)
+					MouseMove, windowX+windowWidth//3-30+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((5*windowHeight)//10 - 52)+SubStr(pos, InStr(pos, ",")+1)
 					sleep, 150
 					Click
 					sleep 100
@@ -8970,7 +8970,7 @@ nm_Reset(checkAll:=1, wait:=2000, convert:=1, force:=0){
 	global youDied
 	global VBState
 	global KeyDelay
-	global SC_E, SC_Esc, SC_R, SC_Enter, RotRight, RotUp, RotDown, ZoomOut
+	global SC_E, SC_Esc, SC_R, SC_Enter, RotRight, RotLeft, ZoomOut
 	global objective
 	global AFBrollingDice
 	global AFBuseGlitter
@@ -9044,9 +9044,9 @@ nm_Reset(checkAll:=1, wait:=2000, convert:=1, force:=0){
 		MouseMove, windowX+350, windowY+offsetY+100
 		;check to make sure you are not in a yes/no prompt
 		WinGetClientPos(windowX, windowY, windowWidth, windowHeight, "ahk_id " hwnd)
-		pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250 "|" windowY+((6*windowHeight)//10 - 60) "|500|150")
+		pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//3-30 "|" windowY+((5*windowHeight)//10 - 52) "|500|150")
 		if (Gdip_ImageSearch(pBMScreen, bitmaps["no"], pos, , , , , 2, , 3) = 1) {
-			MouseMove, windowX+windowWidth//2-250+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((6*windowHeight)//10 - 60)+SubStr(pos, InStr(pos, ",")+1)
+			MouseMove, windowX+windowWidth//3-30+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((5*windowHeight)//10 - 52)+SubStr(pos, InStr(pos, ",")+1)
 			Click
 			MouseMove, windowX+350, windowY+offsetY+100
 		}
@@ -9133,17 +9133,21 @@ nm_Reset(checkAll:=1, wait:=2000, convert:=1, force:=0){
 			Sleep, 1000
 		}
 		SetKeyDelay, PrevKeyDelay
-		sendinput {%RotUp% 4}
-		send {%ZoomOut% 8}
-		sleep,500
-		loop, 4 { ;16
-			If ((nm_imgSearch("hive4.png",20,"actionbar")[1] = 0) || (nm_imgSearch("hive_honeystorm.png",20,"actionbar")[1] = 0) || (nm_imgSearch("hive_snowstorm.png",20,"actionbar")[1] = 0)){
-				sendinput {%RotRight% 4}{%RotDown% 4}
+		sendinput {%RotRight% 3}
+		send {%ZoomOut% 3}
+		sleep, 500
+
+		loop, 4 {
+			pBMScreen := Gdip_BitmapFromScreen(WindowX+WindowWidth/4-250 "|" WindowY+windowHeight/2-50 "|400|250")
+			if (Gdip_ImageSearch(pBMScreen, bitmaps["pBMHive"], , , , , , 10) > 0 || Gdip_ImageSearch(pBMScreen, bitmaps["pBMHiveHS"], , , , , , 5) > 0) {
+				Gdip_DisposeImage(pBMScreen)
+				sendinput {%RotLeft% 3}
 				HiveConfirmed:=1
 				break
 			}
 			sendinput {%RotRight% 4}
 			sleep (250+KeyDelay)
+			Gdip_DisposeImage(pBMScreen)
 		}
 	}
 	;convert
@@ -9210,29 +9214,29 @@ nm_AmuletPrompt(decision:=0){
 	else
 		WinActivate, Roblox
 
-	pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250 "|" windowY "|500|" windowHeight)
+	pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//3-30 "|" windowY "|500|" windowHeight)
 
 	if (Gdip_ImageSearch(pBMScreen, bitmaps["keep"], pos, , , , , 2, , 2) = 1)
 	{
 		switch % decision
 		{
 			case "keep",1:
-			MouseMove, windowX+windowWidth//2-250+SubStr(pos, 1, InStr(pos, ",")-1)+10, windowY+SubStr(pos, InStr(pos, ",")+1)+10, 5
+			MouseMove, windowX+windowWidth//3-30+SubStr(pos, 1, InStr(pos, ",")-1)+10, windowY+SubStr(pos, InStr(pos, ",")+1)+10, 5
 			Click
 			Gdip_DisposeImage(pBMScreen)
 			nm_setShiftLock(Prev_ShiftLock)
 			return 1
 
 			case "replace",2:
-			MouseMove, windowX+windowWidth//2-250+SubStr(pos, 1, InStr(pos, ",")-1)+190, windowY+SubStr(pos, InStr(pos, ",")+1)+10, 5
+			MouseMove, windowX+windowWidth//3-30+SubStr(pos, 1, InStr(pos, ",")-1)+190, windowY+SubStr(pos, InStr(pos, ",")+1)+10, 5
 			Click
 			Gdip_DisposeImage(pBMScreen)
 			Loop, 25
 			{
-				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250 "|" windowY "|500|" windowHeight)
+				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//3-30 "|" windowY "|500|" windowHeight)
 				if (Gdip_ImageSearch(pBMScreen, bitmaps["yes"], pos, , , , , 2, , 2) = 1)
 				{
-					MouseMove, windowX+windowWidth//2-250+SubStr(pos, 1, InStr(pos, ",")-1), windowY+SubStr(pos, InStr(pos, ",")+1), 5
+					MouseMove, windowX+windowWidth//3-30+SubStr(pos, 1, InStr(pos, ",")-1), windowY+SubStr(pos, InStr(pos, ",")+1), 5
 					Click
 					Gdip_DisposeImage(pBMScreen)
 					break
@@ -9272,7 +9276,7 @@ nm_gotoRamp(){
     nm_endWalk()
 }
 nm_gotoCannon(){
-	global LeftKey, RightKey, currentWalk, objective, SC_Space, bitmaps
+	global LeftKey, RightKey, FwdKey, BackKey, currentWalk, objective, SC_Space, bitmaps
 
 	nm_setShiftLock(0)
 
@@ -9291,15 +9295,15 @@ nm_gotoCannon(){
 		movement := "
 		(LTrim Join`r`n
 		" nm_Walk(8, RightKey) "
+		" nm_Walk(8, FwdKey) "
+		" nm_Walk(4, RightKey) "
 		)"
 		nm_createWalk(movement)
 		KeyWait, F14, D T5 L
 		KeyWait, F14, T5 L
 		nm_endWalk()
 		Sleep, 500
-		SendInput {%SC_Space% down}
-		Sleep, 50
-		SendInput {%SC_Space% up}{%RightKey% down}
+		SendInput {%BackKey% down}
 		DllCall("GetSystemTimeAsFileTime","int64p",s)
 		n := s, f := s+100000000
 		while (n < f)
@@ -9313,7 +9317,7 @@ nm_gotoCannon(){
 			Gdip_DisposeImage(pBMScreen)
 			DllCall("GetSystemTimeAsFileTime","int64p",n)
 		}
-		SendInput {%RightKey% up}
+		SendInput {%BackKey% up}
 
 		if (success = 1) ; check that cannon was not overrun, at the expense of a small delay
 		{
@@ -9335,7 +9339,7 @@ nm_gotoCannon(){
 				{
 					movement := "
 					(LTrim Join`r`n
-					" nm_Walk(1.5, LeftKey) "
+					" nm_Walk(1.5, FwdKey) "
 					)"
 					nm_createWalk(movement)
 					KeyWait, F14, D T5 L
@@ -14337,11 +14341,11 @@ nm_BasicEggHatcher()
 		Loop, 10
 		{
 			Sleep, 100
-			pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250 ""|"" windowY+offsetY+((6*windowHeight)//10-60) ""|500|150"")
+			pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//3-30""|"" windowY+offsetY+((5*windowHeight)//10-52) ""|500|150"")
 			if (Gdip_ImageSearch(pBMScreen, bitmaps[""yes""], pos, , , , , 2, , 2) = 1)
 			{
 				Gdip_DisposeImage(pBMScreen)
-				Click % windowX+windowWidth//2-250+SubStr(pos, 1, InStr(pos, "","")-1) "" "" windowY+offsetY+((6*windowHeight)//10-60)+SubStr(pos, InStr(pos, "","")+1)
+				Click % windowX+windowWidth//3-30+SubStr(pos, 1, InStr(pos, "","")-1) "" "" windowY+offsetY+((5*windowHeight)//10-52)+SubStr(pos, InStr(pos, "","")+1)
 				break
 			}
 			Gdip_DisposeImage(pBMScreen)
@@ -15809,7 +15813,7 @@ DisconnectCheck(testCheck := 0)
 			WinActivate, Roblox
 			VarSetCapacity(duration,256),DllCall("GetDurationFormatEx","str","!x-sys-default-locale","uint",0,"ptr",0,"int64",(ReconnectDuration := (nowUnix() - ReconnectStart))*10000000,"wstr","mm:ss","str",duration,"int",256)
 			nm_setStatus("Completed", "Reconnect`nTime: " duration " - Attempts: " i)
-			Sleep, 500
+			Sleep, 7000 ; wait for friend prompt to go away
 
 			LastClock:=nowUnix()
 			IniWrite, %LastClock%, settings\nm_config.ini, Collect, LastClock
@@ -16056,8 +16060,8 @@ nm_claimHiveSlot(){
 		movement := "
 		(LTrim Join`r`n
 		" nm_Walk(35, FwdKey, RightKey) "
-		" nm_Walk(5, BackKey) "
-		" nm_Walk(3.5, LeftKey) "
+		" nm_Walk(3, BackKey) "
+		" nm_Walk(2, LeftKey) "
 		)"
 		nm_createWalk(movement)
 		KeyWait, F14, D T5 L
@@ -16068,7 +16072,7 @@ nm_claimHiveSlot(){
 		slots := {}
 		movement := "
 		(LTrim Join`r`n
-		" nm_Walk(8.35, LeftKey) "
+		" nm_Walk(8.2, LeftKey) "
 		)"
 		Loop % HiveSlot
 		{
@@ -16081,8 +16085,8 @@ nm_claimHiveSlot(){
 			}
 
 			Sleep, 500
-			pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-200 "|" windowY+offsetY+36 "|200|120")
-			if (Gdip_ImageSearch(pBMScreen, bitmaps["e_button"], , , , , , 2, , 6) = 1)
+			pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-200 "|" windowY+offsetY "|400|125")
+			if (Gdip_ImageSearch(pBMScreen, bitmaps["claimhive"], , , , , , 2, , 6) = 1)
 				slots[A_Index] := 1
 			Gdip_DisposeImage(pBMScreen)
 		}
@@ -16095,15 +16099,15 @@ nm_claimHiveSlot(){
 			{
 				movement := "
 				(LTrim Join`r`n
-				" nm_Walk((HiveSlot - slots.MinIndex()) * 8.35, RightKey) "
+				" nm_Walk((HiveSlot - slots.MinIndex()) * 8.2, RightKey) "
 				)"
 				nm_createWalk(movement)
 				KeyWait, F14, D T5 L
 				KeyWait, F14, T20 L
 				nm_endWalk()
 				Sleep, 500
-				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-200 "|" windowY+offsetY+36 "|200|120")
-				if (Gdip_ImageSearch(pBMScreen, bitmaps["e_button"], , , , , , 2, , 6) = 1) {
+				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-200 "|" windowY+offsetY "|400|125")
+				if (Gdip_ImageSearch(pBMScreen, bitmaps["claimhive"], , , , , , 2, , 6) = 1) {
 					Gdip_DisposeImage(pBMScreen)
 					HiveSlot := slots.MinIndex()
 					break
@@ -16119,8 +16123,8 @@ nm_claimHiveSlot(){
 					nm_endWalk()
 
 					Sleep, 500
-					pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-200 "|" windowY+offsetY+36 "|200|120")
-					if (Gdip_ImageSearch(pBMScreen, bitmaps["e_button"], , , , , , 2, , 6) = 1) {
+					pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-200 "|" windowY+offsetY "|400|125")
+					if (Gdip_ImageSearch(pBMScreen, bitmaps["claimhive"], , , , , , 2, , 6) = 1) {
 						Gdip_DisposeImage(pBMScreen)
 						HiveSlot += A_Index
 						break 2
@@ -20284,7 +20288,7 @@ ba_placePlanter(fieldName, planter, planterNum, atField:=0){
 	{
 		WinGetClientPos(windowX, windowY, windowWidth, windowHeight, "ahk_id " hwnd)
 		pBMScreen := Gdip_BitmapFromScreen(windowX "|" windowY+offsetY+150 "|" windowWidth//2 "|" Max(480, windowHeight-offsetY-150))
-
+		
 		if (A_Index = 1)
 		{
 			; wait for red vignette effect to disappear
@@ -20312,7 +20316,7 @@ ba_placePlanter(fieldName, planter, planterNum, atField:=0){
 			}
 		}
 
-		if ((Gdip_ImageSearch(pBMScreen, bitmaps[planterName], planterPos, , , 306, , 10, , 5) = 0) || (Gdip_ImageSearch(pBMScreen, bitmaps["yes"], , windowWidth//2-250, , , , 2, , 2) = 1)) {
+		if ((Gdip_ImageSearch(pBMScreen, bitmaps[planterName], planterPos, , , 306, , 10, , 5) = 0) || (Gdip_ImageSearch(pBMScreen, bitmaps["yes"], , windowWidth//3-30, , , , 2, , 2) = 1)) {
 			Gdip_DisposeImage(pBMScreen)
 			break
 		}
@@ -20325,9 +20329,9 @@ ba_placePlanter(fieldName, planter, planterNum, atField:=0){
 	{
 		WinGetClientPos(windowX, windowY, windowWidth, windowHeight, "ahk_id " hwnd)
 		loop 3 {
-			pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250 "|" windowY+((6*windowHeight)//10 - 60) "|500|150")
+			pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//3-30 "|" windowY+((5*windowHeight)//10 - 52) "|500|150")
 			if (Gdip_ImageSearch(pBMScreen, bitmaps["yes"], pos, , , , , 2, , 2) = 1) {
-				MouseMove, windowX+windowWidth//2-250+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((6*windowHeight)//10 - 60)+SubStr(pos, InStr(pos, ",")+1)
+				MouseMove, windowX+windowWidth//3-30+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((5*windowHeight)//10 - 52)+SubStr(pos, InStr(pos, ",")+1)
 				sleep, 150
 				Click
 				sleep 100
@@ -20448,9 +20452,9 @@ ba_harvestPlanter(planterNum){
 		WinGetClientPos(windowX, windowY, windowWidth, windowHeight, "ahk_id " hwnd)
 		if (HarvestFullGrown = 1) {
 			loop 3 {
-				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250 "|" windowY+((6*windowHeight)//10 - 60) "|500|150")
+				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//3-30 "|" windowY+((5*windowHeight)//10 - 52) "|500|150")
 				if (Gdip_ImageSearch(pBMScreen, bitmaps["no"], pos, , , , , 2, , 3) = 1) {
-					MouseMove, windowX+windowWidth//2-250+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((6*windowHeight)//10 - 60)+SubStr(pos, InStr(pos, ",")+1)
+					MouseMove, windowX+windowWidth//3-30+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((5*windowHeight)//10 - 52)+SubStr(pos, InStr(pos, ",")+1)
 					sleep, 150
 					Click
 					sleep 100
@@ -20464,9 +20468,9 @@ ba_harvestPlanter(planterNum){
 		}
 		else {
 			loop 3 {
-				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250 "|" windowY+((6*windowHeight)//10 - 60) "|500|150")
+				pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//3-30 "|" windowY+((5*windowHeight)//10 - 52) "|500|150")
 				if (Gdip_ImageSearch(pBMScreen, bitmaps["yes"], pos, , , , , 2, , 2) = 1) {
-					MouseMove, windowX+windowWidth//2-250+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((6*windowHeight)//10 - 60)+SubStr(pos, InStr(pos, ",")+1)
+					MouseMove, windowX+windowWidth//3-30+SubStr(pos, 1, InStr(pos, ",")-1), windowY+((5*windowHeight)//10 - 52)+SubStr(pos, InStr(pos, ",")+1)
 					sleep, 150
 					Click
 					sleep 100

nm_image_assets/general/bitmaps.ahk
@@ -60,3 +60,6 @@ Shrine["LoadedDice"] := Gdip_BitmapFromBase64("iVBORw0KGgoAAAANSUhEUgAAABsAAAAZC
 Shrine["SmoothDice"] := Gdip_BitmapFromBase64("iVBORw0KGgoAAAANSUhEUgAAABsAAAAaCAYAAABGiCfwAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAXUSURBVEhLjVVZb1RlGH7OOtNOO9PSTkstxQoSq3ElCkSCJi4XruHGxAsT/UteeuEfkAsTY4xGiCwSwFJkKwWhtCUd21I6+9k3n+87Z+zU1MIzc+bMeb/vvM+7f8oHP3yVvD34JfJ6PxR+OoiTWPwCiirl4qOrKrxHj3D/yi0sV2yuiZ0K8kaMiWEXe8s+3MBATx4Y6It4j6BqKlRFQbXqQV32r+F0/Vt4MV/OEMQelp1bOFn9BjeaJ+FELSkX9LnePHJ5k/8S+UUi7gmoD709AYYHXBJ5MIxILsVxgjBK0GyRzE9crPp3seLchR+7QieCxMHl1vdYcv/ENetHXKx/h3VvCVESQDNNaDmaLiHZUj4RBCWBpiVQ1ZS8Y0sQJKhVHahCJJQ/cG6QLPUuTHysBX9JeTvcIOkV3HcuI4x9KJqG/lIeOpV2gk7jGT6qIoRMEKVUKTwvRq3uCjKxOcJKMAefymknw+jLTWK/SiVR6GMjfEDPQgpUFEk23O/D1CNoasyLHvHq0k9kplBmWwEajYxMEDhxU4ZJIEYk77IsEvFSwjVP7hP+FPpNTE1YmBq3MVl2MTniYKLsyHckBGl2iTDW6vZWMj9poRGuoe6vYMGaSaXMQWzGUHUdA9oebtZISrluyiIZKnkksfHUkINCnl5nSHlSw0LaXWW+LIsF0lmOEGKu9Rsu1U7gjnNWviBXRNIVKtb3crMu85OoJpRcv8ybybI3dBrEfXK/WGc0InI7doiHDy1ebeYt7JAR3LRSn8Ni+zJa8XoqyKApOvq0XVSoSmmkaCQrpovCUwojUeJhAteN0Gx6WF1r4/addVy9WsHD9TbCoItMiRWoLhvQ3+TvQGH4DFWUu0LFDC2fE3qWsOyEFx2Sas3F/PwGZq5U8Pv5JfwxXcHCYp0h9BFFEZTnTkylLvBXiVgM5EpEZWXIq/3Ynz+CNwc+5wTJiZKhYSGU6gM0Zs+g1bBQpyeWHcBzA7hOAN8P2VshfC9gOEPZ2JGS7wojeRKdIekiKqqjeKHnPbzU/770TBAJiMQ7sYm78zXML9RR+buN9XUbdXrWbNiw2i6cto2ApH6sw9LGUMu/3EW2DYb0SRwoHMYuYzyTZBC50/P0KEKDXjXrLdhU7jgebNuDwya2ozyayS40e19Be/AY4uFDzHlm7XaoRRVYUZ3/Nr0VUJgrw+QU6S3AbrXhWhYrz0XD70MzHkDT2I9m+UN4k18gKh+FWhijfRrUHnUwU0ElLJK0iVO0ojUsu7ObE6Ubuoa+oSFohi4fPYa1OfIx3PHjCEbe4lTeLeXdUMdzz2d/+SCq0duMrJgky95NSdgN4aewtDgySjIjk7Fm8yUO6SJUo5frqbwbatnYJ71RQpZ+SKItUeXRQO/uWZfoncunzXCKUBaKJZ5XWiZ5PNRhcy/yCqeBa8gwxpo4tTqgATBYeS20ghp7Kl1L4hiebWGjUkHoiRDTQjb646AO6GM40HMMSR/PnWIgy78DQTVqPovXi8cxmBtlNaUKvVYTt8+exq0zp+C2W2xujTN0M/f/BzWvFTDZ+6ps2CRLlxi4PcoAXuv7BIdLn6Gce1oSiwi3Nx5hjkT3py/SK3ESqHD1UTilN9KXd4CqKQYGjTFO9XGYSi9yDOmw/gwO9n0qm1n0mCEMYQjb1Q3cu3AeizPTcBoNSeSQyCodAgr/6cVtIPvM1HoZyqMk2YfdxhQO9h/Hi8V30aOxstjAMeeaU2/g9rkzWJi+BLtWlfPR1wbRLh1BUphg9aUtsBOUry/MyCSJSlt3l6QXg+aYXBQQobPrNRm6e+fPIXB5SNIAn6dAs/wREnNAtsGT4N+mEh4O5cZRNMqZJEVjdQXXf/4J8wxf6HvcqMFVh9AYfIdEJRLtOPG2YMtOkT9xdknwKNlYWsTsqV+xfPM6fJa6OE4c5rfFWScmROrRlsbcEduaFQcBmmurmD35Cyqz1+Gy1BMa4TF07aIohj1Zjp6cCAD+ATVB4WVhWCOJAAAAAElFTkSuQmCC")
 Shrine["FieldDice"] := Gdip_BitmapFromBase64("iVBORw0KGgoAAAANSUhEUgAAAB0AAAAaCAYAAABLlle3AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAUYSURBVEhLnZbZbhNXGMf/Z3YvCXa22hCyECWUtAKJsrZFpVUvetVKfYG+QC9709fpRR6ghCJxEahEIYlUJRCiKlgtiaAExziLHa+z9fvOzMR2YmjEX5rMeGbO+X37RPx665bveUCt6WH5eR3z6w2wFAFoig9Dd6FrLnxfoGmrqNPh03Of/xySqQlMZ3Vcm4whGdNgmiZ+v38fd+/elc8dX8MbfRoKL27YHl6+sfHvriMfshQCnulv4IcvNvDz9yv46du/8M35N0joZOFb5PFebnBm+bR5F9ugrL6s47fHFdxbq2GzTCtI5CQShoebFzYxPLhLBngwjSbOT7zG9bNF9Fot49qlKUCPqUBTRQBkj7qERFn4u4H1ooNK04cTMCGImk01MPLBrgwv/2bPE1YTl8/mcaq/FrwYilNhEGioR8XogCbhrLd6WqofDZcifNq4QufQilAMPpGoozdeD+8EUeFcnj+l48tzMZzs06CSAawDbw9JieJ/WKZBIQzWHpIvPY/E13GdCui0iVRShUpu8+MI2BUanjvEr+2UY7AdnYqiRQgqWEOjqYV3AiVNgZihSGC7ukEp292hDPrndRJ/rmVRrRvSCD5sV6Eq78XWXky+166mw4DwRyiPepGPSAoZZZFxHbbxNd/g83ZVxUIujaVcBvntJDaLPVjOZTH3OItC2ZDvs2RUqh4Kew4V4iFqKBHmg72MO3mIr378xee8MowrUCff62R1s7OG3imV1kz269ROMfRRBUdi2OLiIu7NzcG2bRlqDxqUz2h68HFjKoar4yblRwHNiq5iewObO8VGvyBP1zaDaRaJoWfGx5HJZCi0QSYFYZXLZyxcnbBwccySxVDYJxe7RIlhSUMgHaPmp7C0VzDnsmb7eL13NDyGYchxGIWYN5d4Zni08kXRll52YUrIcEqjURjHSJpaow3Kl5weK5oK7aKFLWCg4C2iuATba3TDBUCdKNm0hqETGj6n3F0aNTHaryEdVzBA/XlxxKRBb4UrOnV41wNPyzWXBv9RKNvIDozTeBsZ1OW0GSTw9ak4vv4ojk8pNTemLFyjmkgTPFLknezVtrZhBVB6UKB8dINyZWZ6VVyi3KcSQWBYgi574yomTxo4PaDLPEdqD2elUkG9XpeMQIJScWARv8y3woP+8D59CRWfUJH1hyMuEl/xT77HX5U2jgQ41CLrz59j/tEjFAoFWTO8yhc6f095lgr0U3+dJI8MGt46Rcmic4oq9cKwgYmMITc+rhqNBh4/eYKZmRmsrq7K3xxiX6hwzAzkfw5sg0MDocSThVqGb3BY49QibAy30v+JDW82m9je3pag5aUl1Gq1VlgpHw1tEPu9VwJocPf9xRtz3tbX17EwP49isSiBUgSjcQBXiaOUugkvORa2zHuIPeMp47outra2sLKygjt37uDVq1fSgEiusFAzx1Aa+k4ChaKRp7OzRz8P7xDDZKE4DnZ3drD27BlydLB3PF/5PssXGlw1iXL8Y9iJKQgjIYG0A8Ts7du0R/ePbTcxlD3J5/N49PAhNjY2ZC5boulKQFsbQC15Ds34BBSjJ3wW6Fg5lXVLMI9CWd7fxx8PHiCXy6FarcrwHhhM+fOFgf2ei6jGJqHoCapInZa2hgbrWDnlHuMmZxADnz59ilKpJEPJPA6lQ4VSVTPYTV4hD6ehWmkIzToCZL3TUw6lSxuXymUJWlxY6GwDEgNtfQglfQz+iQ8hzFT45G0C/gMgZ0Gl4VBregAAAABJRU5ErkJggg==")
 Shrine["Gumdrops"] := Gdip_BitmapFromBase64("iVBORw0KGgoAAAANSUhEUgAAABwAAAAYCAYAAADpnJ2CAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAToSURBVEhLpVZJjxtFFP567/bumfEsWUQSIoIIoHCKuHDgEpA4oEgc+DcgEY78Ba4cEULiEiQSCYQEN8hCkKIgAcPYnsVu271Vd/G9smcyITOICa9VXe5a3veW71XZ2vj2I60LBctzYLdqgO/CjnzY7K/cC/HmbR/dXQuWxlNiWzZ6rR5qQR1pkWIYD1Go3MxxB2zbpjrfzMtay+IYHL6aEazQ5yoL0NRcaYj+JKiQexzi8FGiuXZ3uofBuI/teBtlqcy4KG/X2jRmFZ1aF47tGLDAC2hEjS8CSuMMlz/WnrollF0tvp4WMStXGSbpxPSVroxnAtCqtQxoI5x7JyK9+Cm/AJeD7C35vQCehhrjWmm8PYmI54UqTEuKBKosOFYhKzLY1SRFNU6gpxlQVtDSmFNdFFBVYbw8MoHHiHhdViV2Jjvoj7fQH/Uxmo2Q5qkBtnVG9GlKACqW/LHpjIAJk8/ekYgegyd5cR0XnuOZ3/sioNlBqHMUBJpmU0MsE1yjz6xfbBJQVRHMgqedx+OHRAA828NSYxnLzWXDxsOg/xQJ8wHgYek6ES6HPVzwOwgtB1V1vIuG8n7N0D50Q5r1L4B8RNcBoCx1yaJXo1W8172Md7ovYokMyzzm9Bg9qlImdMJQCdt/kSc8DOjR+aCLi+EyLkU9dJoN7DTJ0iMAJUTCuuF4gK29LczymSmL42S/XJ4AzLTCj+kmvk4e4SbbL/YIuxsOsjUeDAGJIQ/zZDYyGhImxWKXdpTIunrQQLfeRTNqIvKpZ/2bD7TUnd1gDnwPgeuixhOhYCkU5MuZuIF375zC2V9LlNuxOS0CLzQUj5PYGCDP3IB5avbJExJgpblyQKhpOj0EWCcgvZAdZrmMRQE2VAvv3z2H5x8wjIOYlrZoNXPLvG2Ph8YAIY0oFNB9MDFC5mS9eCrRmGWzRUil/g6xUd6a31XGGpolODPkAV5FaNfbtDo0Hoqi9e4GVnh410muWlAzIQs5J21+btomEnmZIyFYwjxb67c+NCh26JmwcpXEhEQRI4DWSOP6Zwq9rGZIMUr3cG7pAs52njMFb3OteWQPDS+5Rk6agiDSC5OTYoYZ29Z4E9bG9ze0znk9OQ78NuspCKB8BznRxENnc4zVGzfh7KVi1zxPVN6Nunhp/WW0wjZ8x2fuQxPmOBtjMOnj0c5DxOnY7NkXs/f0z5/oKp4hqhy8sX4JL7TX8Lud4Fb+B8azCazNEVY+vgl3OF3sYATohSN3HYEsfouXkjXxUA5ppekZmVvKmOWagpc5EWaZb95/TcvHlWAVV/xVvOb20NTu/F7kLVL26qYXUd4yEm8dcRlhO7MxpOP9RLOVGKQaQ94Be7mLSVXHJLiInaW3Mehcw7DzFts1hvQ7ufFLdJwQ19deweXOafymY3w2uosdxSTPcjS+vIP67YewY2qjR/3oKuy1140BJxV7/5aYlBm+GtzHp5s/4PPd+xiphKVB91shiqvnoc505l5KaNLdxfaTC6t1HlvF2P+Vx7g37eNPFZtv3j2wWIvq3BKqtZb5nidyvudZZJ6YhWhxwKMNhgOkutz89FKHzKdiJHg5/185ALRovTltFn+mDJgwqiAjN2M4W2SsgMoYD/lnlSc8tDx6QiCLfxGtgI2EcR/0Uf/iJwJKTdmoLB5/9VPzDScW4G+680tvd+ounQAAAABJRU5ErkJggg==")
+
+bitmaps["pBMHive"] := Gdip_CreateBitmap(1, 4), pGraphics := Gdip_GraphicsFromImage(bitmaps["pBMHive"]), Gdip_GraphicsClear(pGraphics, 0xff4B3F09), Gdip_DeleteGraphics(pGraphics)
+bitmaps["pBMHiveHS"] := Gdip_CreateBitmap(1, 4), pGraphics := Gdip_GraphicsFromImage(bitmaps["pBMHiveHS"]), Gdip_GraphicsClear(pGraphics, 0xff7B6715), Gdip_DeleteGraphics(pGraphics)

nm_image_assets/reconnect/bitmaps.ahk
@@ -1,2 +1,3 @@
 ﻿bitmaps["loading"] := Gdip_BitmapFromBase64("iVBORw0KGgoAAAANSUhEUgAAAGQAAABkAQMAAABKLAcXAAAAA1BMVEUiV6ixRE8dAAAAE0lEQVR42mMYBaNgFIyCUUBXAAAFeAABSanTpAAAAABJRU5ErkJggg==")
 bitmaps["science"] := Gdip_BitmapFromBase64("iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKAQMAAAC3/F3+AAAAA1BMVEX0qQ0Uw53LAAAACklEQVR42mPACwAAHgAB3XenRQAAAABJRU5ErkJggg==")
+bitmaps["claimhive"] := Gdip_BitmapFromBase64("iVBORw0KGgoAAAANSUhEUgAAADMAAAAUCAMAAAFg0cUAAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAGUExURff/+QAAAJz6Ds8AAAACdFJOU/8A5bcwSgAAAAlwSFlzAAAOwgAADsIBFShKgAAAAGNJREFUKFPtj9sKgDAMQ+3//7TNZbYbok8KgoexLEk32BaBlVDzZA2QTmqBujlzMnnEcwN35tbZVspQ2EqIsseq3iAEw0jJRcPto82UzRMd/r1wShZbaPTAKVls0Yr/zlt3InZhnwMqg6pSYgAAAABJRU5ErkJggg==")

commit 7c31b554abce00cea81066269f4e2129435c5ddc

Fix typo breaking Basic Egg Hatcher
initially caused by 9f02dab50d80ae8ff12d124ab23fc3bd986b000d at new line 14344
failed to spot it when replacing the incorrect coordinates using find and replace in 9f02dab50d80ae8ff12d124ab23fc3bd986b000d
please check through code before pushing, this is not the first typo!

submacros/natro_macro.ahk
@@ -14399,7 +14399,7 @@ nm_BasicEggHatcher()
 		Loop, 10
 		{
 			Sleep, 100
-			pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250""|"" windowY+offsetY+windowHeight//2-52 ""|500|150"")
+			pBMScreen := Gdip_BitmapFromScreen(windowX+windowWidth//2-250 ""|"" windowY+offsetY+windowHeight//2-52 ""|500|150"")
 			if (Gdip_ImageSearch(pBMScreen, bitmaps[""yes""], pos, , , , , 2, , 2) = 1)
 			{
 				Gdip_DisposeImage(pBMScreen)